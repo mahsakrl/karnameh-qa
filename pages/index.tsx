c8bcb7003d9caee0ee0695c530a5770c6d5633c6
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 16:39:20 +0330

    create questions list layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, {useEffect, useState} from "react";
+import Head from "next/head";
+import {useDispatch} from "react-redux";
+import LoginLayout from "layouts/loginLayout/LoginLayout";
+import QuestionCard from "components/questionCard/QuestionCard";
+import axiosInstance from "utils/request/axiosInstance";
+import {Question} from "utils/types/commonTypes";
+import {
+    GET_QUESTIONS_FAIL,
+    GET_QUESTIONS_REQUEST,
+    GET_QUESTIONS_SUCCESS,
+} from "store/actionTypes";
+import generalText from "i18n/generalText";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
+    const dispatch = useDispatch();
+    const [questions, setQuestions] = useState<Question[]>();
+    useEffect(() => {
+        onGetQuestions();
+    }, []);
+    const onGetQuestions = () => {
+        dispatch({type: GET_QUESTIONS_REQUEST});
+
+        axiosInstance
+            .get("/questions")
+            .then(res => {
+                setQuestions(res.data);
+                dispatch({type: GET_QUESTIONS_SUCCESS, payload: res.data});
+            })
+            .catch(err => {
+                dispatch({type: GET_QUESTIONS_FAIL, payload: err});
+            });
+    };
+
+    return (
+        <>
+            <Head>
                 <title>{generalText.title}</title>
                 <meta
                     name="description"
@@ -18,112 +44,20 @@ export default function Home() {
                     name="viewport"
                     content="width=device-width, initial-scale=1, maximum-scale=1"
                 />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
-  )
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <LoginLayout
+                    title={generalText.questionList}
+                    updateQuestions={onGetQuestions}
+                >
+                    {questions?.map(question => (
+                        <div key={question.id}>
+                            <QuestionCard question={question} />
+                        </div>
+                    ))}
+                </LoginLayout>
+            </main>
+        </>
+    );
 }
