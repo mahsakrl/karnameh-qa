8ac4ba449bf198dfeac20980789891e6487579fe
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 16:16:11 +0330

    add store provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import store from "store/store";
 import "styles/globals.scss";
+import type {AppProps} from "next/app";
+import {Provider} from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
