732ce4667cb398b2a137c0165bac0657fb62d01e
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 11:34:00 +0330

    add icons assets

---
+++ assets/images.ts
@@ -0,0 +1,8 @@
+export const ARROW_DOWN = require("assets/images/icons/arrow-down.svg");
+export const COMMENT = require("assets/images/icons/comment.svg");
+export const HAPPY_FACE = require("assets/images/icons/happy-face.svg");
+export const PLUS = require("assets/images/icons/plus.svg");
+export const REMOVE = require("assets/images/icons/remove.svg");
+export const SAD_FACE = require("assets/images/icons/sad-face.svg");
+export const SAD_RED_FACE = require("assets/images/icons/sad-red-face.svg");
+
