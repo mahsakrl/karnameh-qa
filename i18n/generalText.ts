65742091f60c7522966258a0eb8df14390fcc2a4
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 13:40:49 +0330

    add global constant text

---
+++ i18n/generalText.ts
@@ -0,0 +1,25 @@
+ const generalMessages={
+time:"ساعت: ",
+date:"تاریخ: ",
+questionList:"لیست سوالات",
+newQuestion:"سوال جدید",
+showDetails:"مشاهده جزییات",
+questionDetails:"جزییات سوال",
+goodAnswer:"پاسخ خوب بود",
+badAnswer:"پاسخ خوب نبود",
+submitYourAnswer:"پاسخ خود را ثبت کنید",
+writeYourAnswer:"پاسخ خود را بنویسید",
+answerText:"متن پاسخ...",
+sendAnswer:"ارسال پاسخ",
+hint:"محل قرارگیری راهنمای خطا",
+createQuestion:"ایجاد سوال",
+giveup:"انصراف",
+answers:"پاسخ‌ها",
+noAnswer:"هیچ پاسخی تاکنون ثبت نشده",
+questionText:"متن سوال",
+subject:"موضوع"
+
+
+
+}
+export default generalMessages
