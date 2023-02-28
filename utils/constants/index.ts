f852c2ad9a3176f068df8bf9ab73a497fcccd8ad
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 11:59:06 +0330

    add basic constant

---
+++ utils/constants/index.ts
@@ -0,0 +1,8 @@
+export const OPTIONS:Intl.DateTimeFormatOptions={
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: "Asia/Tehran",
+}
+export const FA_IR="fa-IR"
+export const SAMPLE_AVATAR="https://i.pravatar.cc/150?img=30"
+export const SAMPLE_USER="الناز شاکردوست"
