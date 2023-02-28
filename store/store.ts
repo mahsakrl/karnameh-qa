8e953b17d0b12ae95a2f32d24ed0aedad1989cb1
Mahsa Karamloo <mahsa.karamloo94@gmail.com>
2023-02-28 18:15:08 +0330

    add type declration

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,10 @@
 import {  legacy_createStore as createStore,compose } from 'redux'
 import reducer from 'store/reducers';
- 
+declare global {
+    interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+  }
 const initialState = {
     questions: {
         isLoading: null,
